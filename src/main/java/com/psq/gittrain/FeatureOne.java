5ac2ed73826bd8f7d4af70292c411bbda15585c9
pengshiquan <1162210866@qq.com>
2019-01-23 17:37:09 +0800

    功能分支二第二次开发
    开发第二个功能。

--- src/main/java/com/psq/gittrain/FeatureOne.java
+++ src/main/java/com/psq/gittrain/FeatureOne.java
@@ -22,4 +22,16 @@ public class FeatureOne {
     public void helpFeature() {
         System.out.printf("这是一个测试的功能");
     }
+
+    /**
+     * Method: twoFeature
+     * Description: 第二个功能
+     *
+     * @param
+     * @return void
+     */
+    public void twoFeature() {
+        System.out.printf("这是第二个功能的开发");
+        System.out.printf("这个是有问题的代码，代码审核时会出现问题！");
+    }
 }

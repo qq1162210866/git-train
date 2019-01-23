7b38584fd37997baa4a051f4eaacd3103e21d612
pengshiquan <1162210866@qq.com>
2019-01-23 16:38:57 +0800

    提交功能一
    提交功能一

---
+++ src/main/java/com/psq/gittrain/FeatureOne.java
@@ -0,0 +1,25 @@
+package com.psq.gittrain;
+
+/**
+ * FeatureOne.java
+ * Description: 功能模块，第一个功能。
+ *
+ * @author Peng Shiquan
+ * Copyright  2018-2019  创捷运维智能科技有限公司
+ * All rights reserved.
+ * @version: 1.0
+ * Reversion:
+ * 1.0 - 新建
+ */
+public class FeatureOne {
+    /**
+     * Method: helpFeature
+     * Description: 帮助功能
+     *
+     * @param
+     * @return void
+     */
+    public void helpFeature() {
+        System.out.printf("这是一个测试的功能");
+    }
+}

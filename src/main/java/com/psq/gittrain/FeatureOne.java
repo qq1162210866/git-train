ef1b1d01d05d05486a878b6a3ec61467ce33e3b7
qq1162210866 <39085318+qq1162210866@users.noreply.github.com>
2019-01-24 09:30:43 +0800

    Release 1.0 (#7)
    * 提交功能一
    
    提交功能一
    
    * 功能一分支继续开发
    
    功能一分支继续开发，新的功能。
    
    * 删除有问题代码
    
    删除有问题代码
    
    * 功能二分支继续开发 (#2)
    
    * 功能一分支继续开发
    
    功能一分支继续开发，新的功能。
    
    * 删除有问题代码
    
    删除有问题代码
    
    * 功能分支二第二次开发
    
    开发第二个功能。
    
    * 删除有问题代码
    
    删除有问题的代码。
    
    * 功能分支二第二次开发 (#3)
    
    * 功能一分支继续开发
    
    功能一分支继续开发，新的功能。
    
    * 删除有问题代码
    
    删除有问题代码
    
    * 功能分支二第二次开发
    
    开发第二个功能。
    
    * 删除有问题代码
    
    删除有问题的代码。
    
    * 功能分支一开发的第二个功能 (#4)
    
    * 提交功能一
    
    提交功能一
    
    * 功能分支一的第二个功能
    
    增加了功能分支一的第二个功能
    
    * 功能一分支 (#5)
    
    * 提交功能一
    
    提交功能一
    
    * 功能分支一的第二个功能
    
    增加了功能分支一的第二个功能
    
    * 增加根据es查询所有设备的功能 (#6)
    
    根据es查询所有设备的功能
    
    * 增加了帮助文档
    
    增加了帮助文档。可以合并到主分支

---
+++ src/main/java/com/psq/gittrain/FeatureOne.java
@@ -0,0 +1,53 @@
+package com.psq.gittrain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FeatureOne.java
+ * Description: 功能模块，第一个功能。
+ *
+ * @author Peng Shiquan
+ * Copyright  2018-2019  创捷运维智能科技有限公司
+ * All rights reserved.
+ * @version: 1.0
+ * Reversion:
+ * 1.0 - 新建
+ */
+public class FeatureOne {
+    /**
+     * Method: helpFeature
+     * Description: 帮助功能
+     *
+     * @param
+     * @return void
+     */
+    public void helpFeature() {
+        System.out.printf("这是一个测试的功能");
+    }
+
+    /**
+     * Method: oneFeature
+     * Description: 功能分支一的第二个新功能
+     *
+     * @param
+     * @return void
+     */
+    public void oneFeature() {
+        System.out.printf("功能分支一的第二个新功能");
+    }
+
+    /**
+     * Method: listEquipmentsByES
+     * Description: 根据es查询所有的设备列表
+     *
+     * @param
+     * @return java.lang.String
+     */
+    public String listEquipmentsByES() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("风机");
+        stringList.add("照明");
+        return "这是全部的设备" + stringList.toString();
+    }
+}
